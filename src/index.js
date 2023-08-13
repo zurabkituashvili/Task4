309f52031bf8175b69a5842c98c22cd3b1a4c26d
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-08-13 19:28:43 +0400

    a

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
+// axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.baseURL = "https://task4-cyan.vercel.app/";
+
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
 const root = ReactDOM.createRoot(document.getElementById("root"));
