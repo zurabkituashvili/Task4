2ff8c499663d3bd530f403fc4f04f9c8e1ac9da3
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-08-13 20:53:49 +0400

    d

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,14 +5,14 @@ const jwt = require("jsonwebtoken");
 const mysql = require("mysql2");
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 
-// app.use(
-//   cors({
-//     origin: "https://task4-cyan.vercel.app", // Update with your frontend's URL
-//     credentials: true, // Enable sending cookies
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://task4-cyan.vercel.app", // Update with your frontend's URL
+    credentials: true, // Enable sending cookies
+  })
+);
 
 app.use(bodyParser.json());
 
@@ -225,7 +225,7 @@ app.get("/check-user-status", authenticateUser, (req, res) => {
   );
 });
 
-const port = 8000;
+const port = 3306;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
