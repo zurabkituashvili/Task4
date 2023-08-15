bb83ab545f0534847bb38fee806bb3e191382945
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-08-15 21:29:22 +0400

    c

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://backend-teu6.onrender.com/";
+// axios.defaults.baseURL = "http://localhost:8000/";
+
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
 const root = ReactDOM.createRoot(document.getElementById("root"));
