f07755286523680672233fdc299dcfa87affd87e
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-08-15 16:33:30 +0400

    a

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000/";
-axios.defaults.baseURL = "https://name-task4-for.onrender.com";
+axios.defaults.baseURL = "https://name-task4-for.onrender.com/";
 // axios.defaults.baseURL = "https://task4-cyan.vercel.app/";
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
